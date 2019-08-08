78d9a3778b48750a2a5bbf8d384357cc3a63b6db
yanxun <1768695841@qq.com>
2019-08-08 21:29:37 +0800

    @BindView,@OnClick 测试通过

--- app/src/main/java/com/yx/easybutterknife/MainActivity.java
+++ app/src/main/java/com/yx/easybutterknife/MainActivity.java
@@ -3,6 +3,7 @@ package com.yx.easybutterknife;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,19 +12,33 @@ import com.yx.annotations.OnClick;
 import com.yx.easybutterknifelib.EasyButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
     @BindView(R.id.tv)
     TextView tv;
+    @BindView(R.id.tv2)
+    TextView tv2;
+    @BindView(R.id.lv)
+    ListView lv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EasyButterKnife.bind(this);
         tv.setText("詹姆斯");
+        tv2.setText("浓眉");
 
     }
 
     @OnClick({R.id.tv2,R.id.tv})
     public void go(View v){
-        Toast.makeText(this, "gogogogogogogog", Toast.LENGTH_SHORT).show();
+        switch (v.getId()){
+            case R.id.tv:
+                Toast.makeText(this, "詹姆斯", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tv2:
+                Toast.makeText(this, "浓眉", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
+
 }
